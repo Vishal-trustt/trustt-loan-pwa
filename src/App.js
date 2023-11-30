7a8480c5c91d10ce67b48e0e22eb5fbabce66126
Vishal-trustt <vishal.magdum@trustt.com>
2023-11-30 14:49:53 +0530

    login form ready

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import "./App.css";
+import Login from "./Components/login/login";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path=" " element={}/>
+        <Route path="/" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
